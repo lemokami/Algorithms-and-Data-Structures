8dd9285a6aa53f1fe09acad4ed950d30463c90d7
jesswinw <jesswinw@gmail.com>
2020-06-09 11:27:05 +0530

    gets freq of  alphannums with O(n) complexity 😍

---
+++ charCount.js
@@ -0,0 +1,22 @@
+"use strict";
+// This program has O(n) complexity where 'n' is the length of the string
+const charCount = (str) => {
+    let letter;
+    let result = {};
+    for (letter of str) {
+        if (isAlphanum(letter)) {
+            result[letter] = (result[letter] | 0) + 1;
+        }
+    }
+    return result;
+};
+const isAlphanum = (char) => {
+    let code = char.charCodeAt(0);
+    if (!(code > 47 && code < 58) &&
+        !(code > 64 && code < 91) &&
+        !(code > 96 && code < 123)) {
+        return false;
+    }
+    return true;
+};
+console.log(charCount("Chrostopher Nolan"));
