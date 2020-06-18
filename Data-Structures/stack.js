8579d63fd8aad9f42519d1f0a483d4ff6b47161e
jesswinw <jesswinw@gmail.com>
2020-06-19 00:30:30 +0530

    renamed stack (ts made a new file)

---
+++ Data-Structures/stack.js
@@ -0,0 +1,41 @@
+"use strict";
+// Implements stack with linked list
+class item {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+class stack {
+    constructor() {
+        this.first = null;
+        this.last = null;
+        this.size = 0;
+    }
+    push(val) {
+        const newitem = new item(val);
+        if (this.size === 0) {
+            this.first = newitem;
+            this.last = this.first;
+        }
+        else {
+            newitem.next = this.first;
+            this.first = newitem;
+        }
+        return ++this.size;
+    }
+    pop() {
+        if (this.size === 0) {
+            console.log('Stack Empty');
+            return undefined;
+        }
+        const popnode = this.first;
+        if (this.size === 1) {
+            this.last = null;
+        }
+        this.first = this.first.next;
+        this.size--;
+        return popnode.val;
+    }
+}
+const mystack = new stack();
